dd9f809199f39ccf5ae59acfa795cf1c8e02196d
aryaneferreira <aryaneferreira8@gmail.com>
2023-07-26 08:56:37 -0300

    Adding login, register components on routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'login', 
+    component: LoginComponent 
+  },
+  { 
+    path: 'register', 
+    component: RegisterComponent 
+  },
+  { 
+    path: '', 
+    redirectTo: 'login', 
+    pathMatch: 'full' 
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

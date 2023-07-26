50ae900c38c0a78a791c25f8b83420b70d382d56
aryaneferreira <aryaneferreira8@gmail.com>
2023-07-26 08:51:09 -0300

    Adding main menu module and register module on app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
+import { RegisterModule } from './register/register.module';
+import { MainMenuModule } from './main-menu/main-menu.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LoginModule
+    LoginModule,
+    RegisterModule,
+    MainMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
